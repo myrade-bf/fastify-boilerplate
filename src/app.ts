a180535fd17e3dd38112e61d01d2a9cdcbb60945
myrade-bf <0xaliasrowyan@gmail.com>
2024-08-21 04:43:39 +0900

    drop: plugins file

--- src/app.ts
+++ src/app.ts
@@ -14,15 +14,6 @@ const app: FastifyPluginAsync<AppOptions> = async (
 ): Promise<void> => {
   // Place here your custom code!
 
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'plugins'),
-    options: opts,
-  })
 
   // This loads all plugins defined in routes
   // define your routes in one of these
